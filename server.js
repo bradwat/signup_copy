fc3e2175bfd30b450737fd8348720c46194388b8
bradwat <bradwat@live.com>
2016-02-01 18:08:36 -0500

    twitter

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
 });
 
+server.post('/signup', function(req,res){
+    res.redirect('/');
+});
 server.listen(8080, function(){
+
 	console.log("Now listening on port 8080");
 });
